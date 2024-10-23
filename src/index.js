5996593d83968b65297cf3c8a86457f5c46428f3
Iman Motlagh <motlaghi1@udayton.edu>
2024-10-23 17:15:48 +0000

    restructure files

--- src/index.js
+++ src/index.js
@@ -8,22 +8,7 @@ const app = express();
 const auth = require('./routes/auth');
 const protected = require('./routes/protected');
 
-// MongoDB connection
-const uname = "deeterd1";
-const psword = "Xb0x4ly3r";
-const cluster = "messengerdb.sd589";
-const dbname = "";  
-
-const uri = `mongodb+srv://${uname}:${psword}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-const mongoose_settings = { useNewUrlParser: true, useUnifiedTopology: true };
-
-mongoose.connect(uri, mongoose_settings);
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => {
-    console.log("Connected successfully to MongoDB");
-});
+const db = require('./config/db');
 
 // Middleware setup
 app.use(cookieParser());
