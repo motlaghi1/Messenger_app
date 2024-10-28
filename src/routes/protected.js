ef7f60c69754dc2325e35ea9c9defa977b340aab
JustinButler1 <jubutler12@gmail.com>
2024-10-28 19:07:48 -0400

    created Edit Profile page

--- src/routes/protected.js
+++ src/routes/protected.js
@@ -17,9 +17,20 @@ router.get('/protected_page', checkSignIn, (req, res) => {
     res.render('protected_page', { id: req.session.user.id });
 });
 
+// Edit page route
+router.get('/edit_profile', checkSignIn, (req, res) => {
+    res.render('edit_profile', { id: req.session.user.id, name: req.session.user.name, email: req.session.user.email, UDid: req.session.user.UDid });
+    console.log(req.session.user)
+});
+
 // Error handling middleware for protected page
 router.use('/protected_page', (err, req, res, next) => {
     res.render('protected_page', {message: "You cannot view this page unless you are logged in."})
+});
+
+// Error handling middleware for edit profile page
+router.use('/edit_profile', (err, req, res, next) => {
+    res.render('edit_profile', {message: "You cannot view this page unless you are logged in."})
 });
 
 module.exports = router;
