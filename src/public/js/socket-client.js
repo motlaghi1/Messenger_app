ceb24a61f5a1fc378c5198ce4566fed96c3607e1
JustinButler1 <jubutler12@gmail.com>
2024-11-26 16:02:43 -0500

    feat: typing indicator

--- src/public/js/socket-client.js
+++ src/public/js/socket-client.js
@@ -1,30 +1,27 @@
-import { createContactItem, displayMessage , formatMessage } from './chat-helpers.js';
+import { createContactItem, displayMessage , formatMessage, showIsTyping } from './chat-helpers.js';
 import { getCurrentChatId } from './chat.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const socket = io();
     const sendButton = document.getElementById('sendButton');
     const inputField = document.getElementById('messageInput');
     const groupModalButton = document.getElementById('saveGroupButton');
+    let typingStarted = false;
     
+    const response = await fetch('/api/current_user');
+    const currentUser = await response.json();
+
     function sendMessage(message, chatId, userId) {
-        async function getCurrentUser() {
-            const response = await fetch('/api/current_user');
-            const currentUser = await response.json();
-            return currentUser;
-        }
         console.log('\x1b[36m%s\x1b[0m', `socket client chat id: ${chatId}`)
-        getCurrentUser().then((user) => {
-            displayMessage(
-                formatMessage(message, user.name, Date.now(), true),
-            );
-            console.log('\x1b[36m%s\x1b[0m', `chat id: ${chatId}`);
-            if (chatId === 'global') {
-                socket.emit('send-message', message);
-            } else {
-                socket.emit('send-message', message, chatId, user._id);
-            }
-        });
+        displayMessage(
+            formatMessage(message, currentUser.name, Date.now(), true),
+        );
+        console.log('\x1b[36m%s\x1b[0m', `chat id: ${chatId}`);
+        if (chatId === 'global') {
+            socket.emit('send-message', message);
+        } else {
+            socket.emit('send-message', message, chatId, currentUser._id);
+        }
     }
     
     groupModalButton.addEventListener('click', function() {
@@ -58,18 +55,49 @@ document.addEventListener('DOMContentLoaded', () => {
     
     sendButton.addEventListener('click', function() {
         const message = inputField.value.trim();
-        console.log('\x1b[32m%s\x1b[0m', `send button pressed with target user = : ${window.targetUserId}`);
         if (message) {
             sendMessage(message, getCurrentChatId(), window.targetUserId); // TODO clear window.targetUserId and start dm emition
             inputField.value = ''; // Clear the input field
         }
     });
 
+    inputField.addEventListener('input', () => {
+        if (typingStarted && inputField.value === '') {
+            socket.emit('user-typing-change', currentUser, getCurrentChatId(), false)
+            return
+        } else 
+        if (typingStarted) {
+            return
+        }
+        socket.emit('user-typing-change', currentUser, getCurrentChatId(), true)
+        typingStarted = true
+        console.log('Started typing')
+    })
+
+    inputField.addEventListener('blur', () => {
+        typingStarted = false
+        socket.emit('user-typing-change', currentUser, getCurrentChatId(), false)
+        console.log('Ended typing')
+    })
+
     socket.on('response', (user, message, chatId) => {
-        console.log('\x1b[36m%s\x1b[0m', 'Receiving Message');
         if (chatId != getCurrentChatId()) { return } // Doesn't need socket if window not open
+        console.log('\x1b[36m%s\x1b[0m', 'Receiving Message');
         displayMessage(
             formatMessage(message, user.name, Date.now(), false)
         );
     });
+
+    socket.on('user-typing-change', (user, chatId, isTyping) => {
+        if (chatId != getCurrentChatId()) { return } // Doesn't need socket if window not open
+        const typingIndicator = document.querySelector('.typing-indicator.other')
+        if (typingIndicator) typingIndicator.remove();
+
+        if (isTyping) { 
+            console.log(`${user.name} is typing...`)
+            showIsTyping(user)
+        } else {
+            console.log(`${user.name} stopped typing...`)
+        }
+    })
 });
