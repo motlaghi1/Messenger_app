9153ad06770193c9476d88f12e7063a443cd770a
JustinButler1 <jubutler12@gmail.com>
2024-11-26 22:04:18 -0500

    Revert "Merge remote-tracking branch 'origin/sprint2' into sprint2-polishing-lazar"
    This reverts commit ea9f4b582b2ace3df67ab1b0c7c83f1a7a922f9e, reversing
    changes made to b345738f06c18f6160f1609585c5eb91b84030d3.

--- src/public/js/socket-client.js
+++ src/public/js/socket-client.js
@@ -1,27 +1,30 @@
-import { createContactItem, displayMessage , formatMessage, showIsTyping } from './chat-helpers.js';
+import { createContactItem, displayMessage , formatMessage } from './chat-helpers.js';
 import { getCurrentChatId } from './chat.js';
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
     const socket = io();
     const sendButton = document.getElementById('sendButton');
     const inputField = document.getElementById('messageInput');
     const groupModalButton = document.getElementById('saveGroupButton');
-    let typingStarted = false;
     
-    const response = await fetch('/api/current_user');
-    const currentUser = await response.json();
-
     function sendMessage(message, chatId, userId) {
-        console.log('\x1b[36m%s\x1b[0m', `socket client chat id: ${chatId}`)
-        displayMessage(
-            formatMessage(message, currentUser.name, Date.now(), true),
-        );
-        console.log('\x1b[36m%s\x1b[0m', `chat id: ${chatId}`);
-        if (chatId === 'global') {
-            socket.emit('send-message', message);
-        } else {
-            socket.emit('send-message', message, chatId, currentUser._id);
+        async function getCurrentUser() {
+            const response = await fetch('/api/current_user');
+            const currentUser = await response.json();
+            return currentUser;
         }
+        console.log('\x1b[36m%s\x1b[0m', `socket client chat id: ${chatId}`)
+        getCurrentUser().then((user) => {
+            displayMessage(
+                formatMessage(message, user.name, Date.now(), true),
+            );
+            console.log('\x1b[36m%s\x1b[0m', `chat id: ${chatId}`);
+            if (chatId === 'global') {
+                socket.emit('send-message', message);
+            } else {
+                socket.emit('send-message', message, chatId, user._id);
+            }
+        });
     }
     
     groupModalButton.addEventListener('click', function() {
@@ -55,49 +58,18 @@ document.addEventListener('DOMContentLoaded', async () => {
     
     sendButton.addEventListener('click', function() {
         const message = inputField.value.trim();
+        console.log('\x1b[32m%s\x1b[0m', `send button pressed with target user = : ${window.targetUserId}`);
         if (message) {
             sendMessage(message, getCurrentChatId(), window.targetUserId); // TODO clear window.targetUserId and start dm emition
             inputField.value = ''; // Clear the input field
         }
     });
 
-    inputField.addEventListener('input', () => {
-        if (typingStarted && inputField.value === '') {
-            socket.emit('user-typing-change', currentUser, getCurrentChatId(), false)
-            return
-        } else 
-        if (typingStarted) {
-            return
-        }
-        socket.emit('user-typing-change', currentUser, getCurrentChatId(), true)
-        typingStarted = true
-        console.log('Started typing')
-    })
-
-    inputField.addEventListener('blur', () => {
-        typingStarted = false
-        socket.emit('user-typing-change', currentUser, getCurrentChatId(), false)
-        console.log('Ended typing')
-    })
-
     socket.on('response', (user, message, chatId) => {
-        if (chatId != getCurrentChatId()) { return } // Doesn't need socket if window not open
         console.log('\x1b[36m%s\x1b[0m', 'Receiving Message');
+        if (chatId != getCurrentChatId()) { return } // Doesn't need socket if window not open
         displayMessage(
             formatMessage(message, user.name, Date.now(), false)
         );
     });
-
-    socket.on('user-typing-change', (user, chatId, isTyping) => {
-        if (chatId != getCurrentChatId()) { return } // Doesn't need socket if window not open
-        const typingIndicator = document.querySelector('.typing-indicator.other')
-        if (typingIndicator) typingIndicator.remove();
-
-        if (isTyping) { 
-            console.log(`${user.name} is typing...`)
-            showIsTyping(user)
-        } else {
-            console.log(`${user.name} stopped typing...`)
-        }
-    })
 });
